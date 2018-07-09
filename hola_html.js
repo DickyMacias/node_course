fcf046caf57b4989cc0c624b5b4c27e8c5c8bbe8
Dicky Macias <maciasfusco@gmail.com>
2018-07-09 17:43:31 -0600

    Modulo 3

---
+++ hola_html.js
@@ -0,0 +1,10 @@
+var http = require("http"),
+    fs = require("fs");
+
+http.createServer(function(req,res){
+  fs.readFile("./index.html", function(err,html){
+    res.writeHead(400,{"Content-Type":"application/json"})
+    res.write(JSON.stringify({nombre: "Dicky Macias", username:"dicky"}));
+    res.end();
+  });
+}).listen(8080);
